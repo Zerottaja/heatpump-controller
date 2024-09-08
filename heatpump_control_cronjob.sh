f55160f59952f5aec2491a6475e750d7f4c271e2
Samu Ampio <samu.ampio@gmail.com>
2024-09-08 21:03:25 +0300

    Add comments to heatpump_control_cronjob.sh

--- heatpump_control_cronjob.sh
+++ heatpump_control_cronjob.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 #!/home/pi/src/heatpump-controller/venv/bin/python3
+
+# this cronjob should be configured in crontask to be run hourly:
+#  1 *  *   *   *     /path/to/source/heatpump-controller/heatpump_control_cronjob.sh
 python3 /home/pi/src/heatpump-controller/main.py
