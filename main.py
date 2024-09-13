816732aa64b9492538138a855f2abe8c4ed320c8
Samu Ampio <samu.ampio@gmail.com>
2024-09-13 13:43:53 +0300

    Modify main.py to allow selection between control sources.

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import sys
+import configparser
 import spot_hinta_api_interface
 import melcloud_interface
+import heating_controller
+
+conf = configparser.ConfigParser()
+conf.read('config.ini')
+
 
 def main():
-    heatpump_request = spot_hinta_api_interface.fetch_control()
+    match conf['DEFAULT']['controlsource']:
+        case 'spot-hinta':
+            heatpump_request = spot_hinta_api_interface.fetch_control()
+        case 'internal':
+            heatpump_request = heating_controller.calculate_control()
     melcloud_interface.set_heatpump_state(heatpump_request)
 
     return
