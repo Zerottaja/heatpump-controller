968fc94f093a125b24eed0405755770349369c3f
Samu Ampio <samu.ampio@gmail.com>
2024-09-13 13:41:56 +0300

    Modify Elering API interface

--- elering_api_interface.py
+++ elering_api_interface.py
@@ -1,9 +1,8 @@
 import configparser
 import requests
 import json
-import time
-from datetime import date
 from datetime import datetime
+from datetime import timezone
 from datetime import timedelta
 
 conf = configparser.ConfigParser()
@@ -17,7 +16,8 @@ def fetch_nps_price_data(fetch_today_also=False):
         no_of_requests = 1
     for i in range(no_of_requests):
         # Define the request URL, all times in UTC
-        request_date_str = (date.today()+timedelta(days=1-i)).strftime('%Y-%m-%d')
+        request_date_str = (datetime.now(timezone.utc) + \
+                            timedelta(days=1-i)).strftime('%Y-%m-%d')
         start_param = request_date_str+'T00%3A00%3A00.000Z'
         end_param = request_date_str+'T23%3A59%3A59.999Z'
         url = 'https://dashboard.elering.ee/api/nps/price?start={}&end={}'\
@@ -28,14 +28,15 @@ def fetch_nps_price_data(fetch_today_also=False):
 
         # Check if the request was successful
         if response.status_code == 200:
-            hours = json.loads(response.content)
-            hours = hours['data'][conf['DEFAULT']['pricearea']]
-            # Define the local file name to save the CSV data
+            raw_data = json.loads(response.content)
+            data_list = raw_data['data'][conf['DEFAULT']['pricearea']]
+            clean_data = {'data': data_list}
+            # Define the local file name to save the JSON data
             filename = './nps-data/nps_price_data_'+request_date_str+'.json'
 
             # Save the content to a local file
             with open(filename, 'w') as file:
-                file.write(json.dumps(hours))
+                file.write(json.dumps(clean_data))
 
             print(f"NPS data successfully saved as {filename}")
         else:
