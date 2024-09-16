93ba8e484d7ea4c86dd588a0cad1f592479d60d0
Samu Ampio <samu.ampio@gmail.com>
2024-09-16 14:48:35 +0300

    Remove files from old locations

--- heatpump_control_cronjob.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#!/path/to/my/venv/bin/python3
-
-# this cronjob should be configured in crontab to be run hourly one minute after hour change:
-#  1 *  *   *   *     /path/to/source/heatpump-controller/heatpump_control_cronjob.sh
-python3 /path/to/my/main.py
--- nps_data_cronjob.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#!/path/to/my/venv/bin/python3
-
-# This cronjob should be configured in crontab to be run once a day
-# after Nord Pool has published tomorrow's Spot price data:
-#  0 18 *   *   *     /path/to/source/heatpump-controller/nps_data_cronjob.sh
-python3 /path/to/my/nps_data_fetcher.py
